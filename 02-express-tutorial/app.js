25be928d03cffa9c3c6c43657518fd71ae1486f7
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2024-08-30 12:56:45 -0500

    Start boilerplate for an express project

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,14 @@
-console.log('Express Tutorial')
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  // res.send("hello world express server");
+  res.send();
+});
+
+app.listen("3000", () => {
+  console.log("Server running on port: 3000");
+});
