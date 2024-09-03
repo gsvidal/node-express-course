6654d10a563d49f9b8021a39008adb325e785235
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2024-09-03 14:34:37 -0500

    Add some json responses for get request

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { products } = require("./data"); 
 
 const app = express();
 
@@ -8,6 +9,19 @@ app.get("/", (req, res) => {
   // res.send("hello world express server");
   res.send();
 });
+
+app.get('/api/v1/test',(req, res) => {
+  res.json({ message: "It worked"});
+})
+
+app.get('/api/v1/products',(req, res) => {
+  res.json({ products : products});
+})
+
+
+app.all('*', (req, res) => {
+  res.send('<h2>Page not found</h2>');
+})
 
 app.listen("3000", () => {
   console.log("Server running on port: 3000");
