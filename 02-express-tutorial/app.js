7ce38e1b827c6e4add18bab1bc02b12e21526d6d
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2024-09-03 15:12:14 -0500

    Add productId and query endpoints for searching by product id or a term or limit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { products } = require("./data"); 
+const { products } = require("./data");
 
 const app = express();
 
@@ -10,18 +10,43 @@ app.get("/", (req, res) => {
   res.send();
 });
 
-app.get('/api/v1/test',(req, res) => {
-  res.json({ message: "It worked"});
-})
+app.get("/api/v1/test", (req, res) => {
+  res.json({ message: "It worked" });
+});
 
-app.get('/api/v1/products',(req, res) => {
-  res.json({ products : products});
-})
+app.get("/api/v1/products", (req, res) => {
+  res.json({ products: products });
+});
 
+app.get("/api/v1/query", (req, res) => {
+  console.log("req.query", req.query);
+  const character = req.query.search;
+  const limit = parseInt(req.query.limit);
 
-app.all('*', (req, res) => {
-  res.send('<h2>Page not found</h2>');
-})
+  const filteredProducts = products.filter((product) =>
+    product.name.startsWith(character)
+  );
+  if (filteredProducts.length > 0) {
+    res.status(200).json({ products: filteredProducts.slice(0, limit) });
+  } else {
+    res.status(404).json({ message: "There are no products" });
+  }
+});
+
+app.get("/api/v1/product/:productId", (req, res) => {
+  const id = Number(req.params.productId);
+  console.log("req.params", req.params);
+  const product = products.find((product) => product.id === id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: "No product with that id" });
+  }
+});
+
+app.all("*", (req, res) => {
+  res.send("<h2>Page not found</h2>");
+});
 
 app.listen("3000", () => {
   console.log("Server running on port: 3000");
